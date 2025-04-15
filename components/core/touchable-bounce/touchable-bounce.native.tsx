79574a93ac81ab1ea27f17e9034ee6d7f81a149d
Oliver Pan <2216991777@qq.com>
2025-04-15 11:49:22 -0500

    add touchable bounce

---
+++ components/core/touchable-bounce/touchable-bounce.native.tsx
@@ -0,0 +1,5 @@
+'use client';
+// @ts-expect-error not typed
+import TouchableBounce from 'react-native/Libraries/Components/Touchable/TouchableBounce';
+
+export { TouchableBounce };
---
+++ components/core/touchable-bounce/touchable-bounce.tsx
@@ -0,0 +1,5 @@
+'use client';
+
+import { TouchableOpacity } from 'react-native';
+
+export { TouchableOpacity as TouchableBounce };
